5c0319e327085112ce87922782de421f8f40c9a4
Phil Gibson <philg@cheatcodegames.net>
2023-08-12 00:18:55 +0100

    feat: kotlin migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,52 +1,54 @@
 plugins {
-    id 'java'
-    id 'org.jetbrains.kotlin.jvm' version '1.9.0'
+    java
+    kotlin("jvm") version "1.9.0"
 }
 
-group = 'uk.co.redeyetechnologies'
-version = '1.0.0'
+group = "uk.co.redeyetechnologies"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
-    maven {
-        name = "papermc-repo"
-        url = "https://repo.papermc.io/repository/maven-public/"
+    maven("papermc-repo") {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
     }
-    maven {
-        name = "sonatype"
-        url = "https://oss.sonatype.org/content/groups/public/"
+    maven("sonatype") {
+        url = uri("https://oss.sonatype.org/content/groups/public/")
     }
 }
 
 dependencies {
-    compileOnly "io.papermc.paper:paper-api:1.20-R0.1-SNAPSHOT"
-    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
+    compileOnly("io.papermc.paper:paper-api:1.20-R0.1-SNAPSHOT")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
-def targetJavaVersion = 17
+val targetJavaVersion = 17
+
 java {
-    def javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
     sourceCompatibility = javaVersion
     targetCompatibility = javaVersion
     if (JavaVersion.current() < javaVersion) {
-        toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
     }
 }
 
-tasks.withType(JavaCompile).configureEach {
-    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible()) {
-        options.release = targetJavaVersion
+tasks.withType<JavaCompile>().configureEach {
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+        options.release.set(targetJavaVersion)
     }
 }
 
-processResources {
-    def props = [version: version]
-    inputs.properties props
-    filteringCharset 'UTF-8'
-    filesMatching('plugin.yml') {
-        expand props
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
     }
 }
+
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain {
+        (this as KotlinJvmToolchain).jdkHome = 11.toString()
+    }
 }
