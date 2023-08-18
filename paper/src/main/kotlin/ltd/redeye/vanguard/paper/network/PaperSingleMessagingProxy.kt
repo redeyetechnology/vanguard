4dac9908eff92776f5477883dbec3746134112d7
Oliver Whitehead <xfawks@gmail.com>
2023-08-18 17:05:53 +0100

    fix(formatting): fix formatting of PaperSingleMessagingProxy

--- paper/src/main/kotlin/ltd/redeye/vanguard/paper/network/PaperSingleMessagingProxy.kt
+++ paper/src/main/kotlin/ltd/redeye/vanguard/paper/network/PaperSingleMessagingProxy.kt
@@ -29,11 +29,11 @@ import java.util.*
 /**
  * A messaging proxy for when no message broker is available
  */
-object PaperSingleMessagingProxy: MessagingProxy {
+object PaperSingleMessagingProxy : MessagingProxy {
 
     override fun alertPlayer(uuid: UUID, message: VanguardMessage, placeholders: TagResolver?) {
         val player = Bukkit.getPlayer(uuid)
-        if(player != null) {
+        if (player != null) {
             message.send(player, placeholders)
         }
     }
@@ -44,7 +44,7 @@ object PaperSingleMessagingProxy: MessagingProxy {
 
     override fun alertStaff(message: VanguardMessage, placeholders: TagResolver?) {
         Bukkit.getOnlinePlayers().forEach { player ->
-            if(player.hasPermission(Permissions.STAFF.permission())) {
+            if (player.hasPermission(Permissions.STAFF.permission())) {
                 message.send(player, placeholders)
             }
         }
