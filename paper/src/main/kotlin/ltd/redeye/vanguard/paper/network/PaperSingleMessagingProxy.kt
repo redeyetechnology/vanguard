66f61728448d3301e0f393429f8c3c69115206ba
Oliver Whitehead <xfawks@gmail.com>
2023-08-18 17:02:15 +0100

    fix(message): Add Placeholders into the MessagingProxy

--- paper/src/main/kotlin/ltd/redeye/vanguard/paper/network/PaperSingleMessagingProxy.kt
+++ paper/src/main/kotlin/ltd/redeye/vanguard/paper/network/PaperSingleMessagingProxy.kt
@@ -4,6 +4,7 @@ import ltd.redeye.vanguard.common.message.VanguardMessage
 import ltd.redeye.vanguard.common.network.messaging.MessagingProxy
 import ltd.redeye.vanguard.common.util.Permissions
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
 import org.bukkit.Bukkit
 import java.util.*
 
@@ -12,10 +13,10 @@ import java.util.*
  */
 object PaperSingleMessagingProxy: MessagingProxy {
 
-    override fun alertPlayer(uuid: UUID, message: VanguardMessage) {
+    override fun alertPlayer(uuid: UUID, message: VanguardMessage, placeholders: TagResolver?) {
         val player = Bukkit.getPlayer(uuid)
         if(player != null) {
-            message.send(player)
+            message.send(player, placeholders)
         }
     }
 
@@ -23,10 +24,10 @@ object PaperSingleMessagingProxy: MessagingProxy {
         Bukkit.getPlayer(player)?.kick(message)
     }
 
-    override fun alertStaff(message: VanguardMessage) {
+    override fun alertStaff(message: VanguardMessage, placeholders: TagResolver?) {
         Bukkit.getOnlinePlayers().forEach { player ->
             if(player.hasPermission(Permissions.STAFF.permission())) {
-                message.send(player)
+                message.send(player, placeholders)
             }
         }
     }
